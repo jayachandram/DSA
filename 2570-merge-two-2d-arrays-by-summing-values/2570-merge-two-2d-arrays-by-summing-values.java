61997da8cd5c06b240beab764c21a8c19b934c1a
Jaya Chandra Mandapaka <153902034+jayachandram@users.noreply.github.com>
2025-03-02 23:33:34 +0530

    Time: 0 ms (100%), Space: 45.6 MB (17.81%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.java
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.java
@@ -1,58 +1,28 @@
+import java.util.*;
+
 class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-        int n = nums1.length, m = nums2.length, size = 0, i = 0, j = 0, k = 0;;
+        int n = nums1.length, m = nums2.length;
+        List<int[]> result = new ArrayList<>();
+        int i = 0, j = 0;
 
-        while(i < n && j < m)
-        {
-            if(nums1[i][0] == nums2[j][0])
-            {
-                size++;
+        while (i < n && j < m) {
+            if (nums1[i][0] == nums2[j][0]) {
+                result.add(new int[]{nums1[i][0], nums1[i][1] + nums2[j][1]});
                 i++;
                 j++;
-            }
-            else if(nums1[i][0] < nums2[j][0])
+            } else if (nums1[i][0] < nums2[j][0]) {
+                result.add(nums1[i]);
                 i++;
-            else
-                j++;
-        }
-
-        int[][] ans = new int[n + m - size][2];
-        i = 0;
-        j = 0;
-        while(i < n && j < m)
-        {
-            if(nums1[i][0] == nums2[j][0])
-            {
-                ans[k][0] = nums1[i][0];
-                ans[k][1] = nums1[i][1] + nums2[j][1];
-                i++;
+            } else {
+                result.add(nums2[j]);
                 j++;
             }
-            else if(nums1[i][0] < nums2[j][0])
-            {
-                ans[k][0] = nums1[i][0];
-                ans[k][1] = nums1[i][1];
-                i++;
-            }
-            else
-            {
-                ans[k][0] = nums2[j][0];
-                ans[k][1] = nums2[j][1];
-                j++;
-            }
-            k++;
-        }
-        while(i < n)
-        {
-            ans[k][0] = nums1[i][0];
-            ans[k++][1] = nums1[i++][1];
-        }
-        while(j < m)
-        {
-            ans[k][0] = nums2[j][0];
-            ans[k++][1] = nums2[j++][1];
         }
 
-        return ans;
+        while (i < n) result.add(nums1[i++]);
+        while (j < m) result.add(nums2[j++]);
+
+        return result.toArray(new int[result.size()][]);
     }
 }
