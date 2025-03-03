3935046f3f955d98d5d9a1ad3030371a1160b898
Jaya Chandra Mandapaka <153902034+jayachandram@users.noreply.github.com>
2025-03-03 21:46:20 +0530

    Time: 8 ms (99.95%), Space: 60.3 MB (17.62%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -1,37 +1,43 @@
 class Solution {
     public int longestSubarray(int[] arr, int x) {
-        Deque<Integer> minDq = new LinkedList<>();
-        Deque<Integer> maxDq = new LinkedList<>();
+        return _longestSubarray(arr, x);
+    }
+
+    private int _longestSubarray(int[] arr, int x) {
+        int[] maxDq = new int[arr.length]; // Stores indices in decreasing order
+        int[] minDq = new int[arr.length]; // Stores indices in increasing order
+        int maxFront = 0, maxRear = -1;
+        int minFront = 0, minRear = -1;
         int currentIndex = 0, maxLength = 0, startIndex = 0;
-        
-        for(int i = 0; i < arr.length; i++)
-        {
-            while(!maxDq.isEmpty() && arr[maxDq.peekLast()] <= arr[i])
-                maxDq.pollLast();
-            maxDq.addLast(i);
-            
-            while(!minDq.isEmpty() && arr[minDq.peekLast()] >= arr[i])
-                minDq.pollLast();
-            minDq.addLast(i);
-            
-            
-            while(arr[maxDq.peekFirst()] - arr[minDq.peekFirst()] > x)
-            {
+
+        for (int i = 0; i < arr.length; i++) {
+            // Maintain decreasing order for maxDq
+            while (maxRear >= maxFront && arr[maxDq[maxRear]] <= arr[i])
+                maxRear--;
+            maxDq[++maxRear] = i;
+
+            // Maintain increasing order for minDq
+            while (minRear >= minFront && arr[minDq[minRear]] >= arr[i])
+                minRear--;
+            minDq[++minRear] = i;
+
+            // Shrink window if difference exceeds limit
+            while (arr[maxDq[maxFront]] - arr[minDq[minFront]] > x) {
                 currentIndex++;
-                
-                if(maxDq.peekFirst() < currentIndex)
-                    maxDq.pollFirst();
- 
-                if(minDq.peekFirst() < currentIndex)
-                    minDq.pollFirst();                   
+
+                if (maxDq[maxFront] < currentIndex)
+                    maxFront++;
+
+                if (minDq[minFront] < currentIndex)
+                    minFront++;
             }
-            
-            if(i - currentIndex + 1 > maxLength)
-            {
-                maxLength =  i - currentIndex + 1;
+
+            if (i - currentIndex + 1 > maxLength) {
+                maxLength = i - currentIndex + 1;
                 startIndex = currentIndex;
             }
         }
+
         return maxLength;
     }
 }
