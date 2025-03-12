63f789448fe96f90ef5d8a919bd0e7a3debc5ca4
Jaya Chandra Mandapaka <153902034+jayachandram@users.noreply.github.com>
2025-03-12 22:36:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Min Cost Climbing Stairs/min-cost-climbing-stairs.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            String[] str = br.readLine().trim().split(" ");
+            int n = str.length;
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+            Solution sln = new Solution();
+            int res = sln.minCostClimbingStairs(arr);
+            System.out.println(res);
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//Back-end complete function Template for Java
+
+class Solution {
+    static int minCostClimbingStairs(int[] cost) {
+        // Write your code here
+        
+        for(int i = cost.length - 3; i > -1; i--)
+        {
+            cost[i] = Math.min(cost[i+1], cost[i + 2] ) + cost[i];
+        return Math.min(cost[0], cost[1]);
+    }
+};
