05f01bfc0061fcbdf1ad9c41f9b0be35672ab959
Jaya Chandra Mandapaka <153902034+jayachandram@users.noreply.github.com>
2025-03-23 20:46:08 +0530

    Time: 60 ms (5.33%), Space: 45.5 MB (5.28%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,32 +1,32 @@
 class Solution {
     public int numDecodings(String digits) {
-        int[] dp = new int[digits.length()];
-
-        return f(digits, 0, digits.length(), dp);
+int[] dp = new int[digits.length()];
+        
+        return f(digits, 0, dp);
         
     }
-    private int f(String digits, int index, int n, int[] dp)
+    private int f(String digits, int index, int[] dp)
     {
-        if(index == n)
+        if(index == digits.length())
             return 1;
         
         if(digits.charAt(index) == '0')
             return 0;
-        
+            
         if(dp[index] != 0)
             return dp[index];
         
-        int ways = f(digits, index + 1, n, dp);
+        int ways = f(digits, index + 1, dp);
         
-        if(index < n-1)
+        if(index < digits.length() -1)
         {
             int num = Integer.parseInt(digits.substring(index, index + 2));
             
             if(num >= 10 && num <= 26)
-                ways += f(digits, index + 2, n, dp);
+                ways += f(digits, index + 2, dp);
         }
         
-        return dp[index] = ways;
+        return dp[index]= ways;
         
     }
 }
