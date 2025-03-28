552fc2ec1daeb20cdbbb0552fc987aaedbce8fa3
Jaya Chandra Mandapaka <153902034+jayachandram@users.noreply.github.com>
2025-03-28 23:11:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Platforms/minimum-platforms.java
+++ Difficulty: Medium/Minimum Platforms/minimum-platforms.java
@@ -41,35 +41,27 @@ class Main {
 // User function Template for Java
 
 class Solution {
-    // Function to find the minimum number of platforms required at the
-    // railway station such that no train waits.
-   static int findPlatform(int arr[], int dep[]) {
-        int n = arr.length;
-        int[][] newArr = new int[n][2];
-
-        for (int i = 0; i < n; i++) {
-            newArr[i][0] = arr[i];
-            newArr[i][1] = dep[i];
-        }
-
-        // Sort based on arrival time
-        Arrays.sort(newArr, (a, b) -> Integer.compare(a[0], b[0]));
-
-        // Priority Queue (Min Heap) to store departure times
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-
-        // Add the first train's departure
-        pq.add(newArr[0][1]);
-
-        for (int i = 1; i < n; i++) {
-            // If the current train arrives after the earliest departure, remove it
-            if (newArr[i][0] > pq.peek()) {
-                pq.poll();
+    static int findPlatform(int arr[], int dep[]) {
+       
+        Arrays.sort(arr);
+        Arrays.sort(dep);
+        
+        int  n = arr.length;
+        int maxPlatform = 1;
+        int platform = 1;
+        int i = 1, j = 0;
+        
+        while(i<n && j<n){
+            if(arr[i]<=dep[j]){
+                platform++;
+                i++;
+            }else{
+                platform--;
+                j++;
             }
-            pq.add(newArr[i][1]); // Add current train's departure
+            maxPlatform = Math.max(maxPlatform, platform);
         }
-
-        // The size of the priority queue will be the minimum number of platforms needed
-        return pq.size();
+        
+        return maxPlatform;
     }
 }
