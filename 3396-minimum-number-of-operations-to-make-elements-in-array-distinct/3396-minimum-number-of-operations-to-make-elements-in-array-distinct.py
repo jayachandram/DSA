eeabd2f8b591ec32fecbf7e3660a87acf9785991
Jaya Chandra Mandapaka <153902034+jayachandram@users.noreply.github.com>
2025-04-08 21:03:35 +0530

    Time: 3 ms (69.84%), Space: 17.6 MB (93.16%) - LeetHub

--- 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.py
+++ 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.py
@@ -1,38 +1,69 @@
 from collections import Counter
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
+        l = len(nums)
+        if(len(nums) < 2):
+            return 0
+        if(len(nums) == 2):
+            if(nums[0] != nums[1]):
+                return 0
+            return 1
+        if(len(nums) == 3):
 
-        minOperations = 0
-        boolean = False
+            for i in range(3):
+                for j in range(3):
+                    if i != j and nums[i] == nums[j]:
+                        return 1
+            return 0
+        s = set()
 
-        while True:
-            boolean = False
-            if(len(nums) < 2):
-                return minOperations
-            if(len(nums) == 2):
-                if(nums[0] != nums[1]):
-                    return minOperations
-                minOperations += 1
-                return minOperations
-            if(len(nums) == 3):
-
-                for i in range(3):
-                    for j in range(3):
-                        if i != j and nums[i] == nums[j]:
-                            minOperations += 1
-                            return minOperations
-                return minOperations
-
-            d = Counter(nums)
-
-            for i in d:
-                if d[i] > 1:
-                    boolean = True
-            
-            if boolean:
-                nums= nums[3:]
-                minOperations += 1
+        for i in reversed(nums):
+            l -= 1
+            if i in s:
+                return (l + 3)//3
             else:
-                return minOperations
+                s.add(i)
+
+        return 0
+
+
+
+
+# from collections import Counter
+# class Solution:
+#     def minimumOperations(self, nums: List[int]) -> int:
+
+#         minOperations = 0
+#         boolean = False
+
+#         while True:
+#             boolean = False
+#             if(len(nums) < 2):
+#                 return minOperations
+#             if(len(nums) == 2):
+#                 if(nums[0] != nums[1]):
+#                     return minOperations
+#                 minOperations += 1
+#                 return minOperations
+#             if(len(nums) == 3):
+
+#                 for i in range(3):
+#                     for j in range(3):
+#                         if i != j and nums[i] == nums[j]:
+#                             minOperations += 1
+#                             return minOperations
+#                 return minOperations
+
+#             d = Counter(nums)
+
+#             for i in d:
+#                 if d[i] > 1:
+#                     boolean = True
+            
+#             if boolean:
+#                 nums= nums[3:]
+#                 minOperations += 1
+#             else:
+#                 return minOperations
         
-        return minOperations
+#         return minOperations
