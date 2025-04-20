bfca6fae4480425ebbb2a202d70b786664b9652b
Jaya Chandra Mandapaka <153902034+jayachandram@users.noreply.github.com>
2025-04-20 09:00:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find Only Repetitive Element from 1 to n-1/find-only-repetitive-element-from-1-to-n1.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            // Read the array from input line
+            String inputLine = read.readLine().trim();
+            String[] inputArr = inputLine.split("\\s+");
+            int n = inputArr.length;
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputArr[i]);
+            }
+
+            Solution ob = new Solution();
+            System.out.println(ob.findDuplicate(arr));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public int findDuplicate(int[] arr) {
+        // code here
+        int num = 0;
+        for(int i = 1; i < arr.length; i++)
+            num ^= i ^ arr[i-1];
+        return num ^ arr[arr.length-1];
+    }
+}
