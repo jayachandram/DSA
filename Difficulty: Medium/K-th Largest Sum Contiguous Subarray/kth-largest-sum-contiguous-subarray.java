1df5a81c79bd71bb1bd5b2c99c54c40264adcf9d
Jaya Chandra Mandapaka <153902034+jayachandram@users.noreply.github.com>
2025-05-11 09:05:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K-th Largest Sum Contiguous Subarray/kth-largest-sum-contiguous-subarray.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+
+class Solution {
+    public static int kthLargest(int[] arr, int k) {
+        // code here
+        
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+
+        
+        for(int i = 0; i < arr.length; i++)
+        {
+            int sum = 0;
+            
+            for(int j = i; j < arr.length; j++)
+            {
+                sum += arr[j];
+            
+                heap.add(sum);
+                
+                while(heap.size() > k)
+                    heap.poll();
+            }
+        }
+        
+        return heap.peek();
+    }
+}
+
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+            int k = Integer.parseInt(br.readLine());
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            Solution obj = new Solution();
+            int res = obj.kthLargest(arr, k);
+
+            System.out.println(res);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
