a1d8e7345c1b250a4ca526724fd5e6e5f9ed6a65
Jaya Chandra Mandapaka <153902034+jayachandram@users.noreply.github.com>
2025-05-14 22:40:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Look and Say Pattern/look-and-say-pattern.java
+++ Difficulty: Medium/Look and Say Pattern/look-and-say-pattern.java
@@ -1,65 +1,52 @@
 //{ Driver Code Starts
-//Initial Template for Java
-
+// Initial Template for Java
 import java.io.*;
-import java.util.*; 
+import java.util.*;
 
-class GFG{
-    public static void main(String args[]) throws IOException { 
+class GFG {
+    public static void main(String args[]) throws IOException {
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(read.readLine());
-        
-        while(t-- > 0){
+
+        while (t-- > 0) {
             int n = Integer.parseInt(read.readLine());
-            
-            
 
             Solution ob = new Solution();
-            
-            System.out.println(ob.lookandsay(n));
-        
-System.out.println("~");
+
+            System.out.println(ob.countAndSay(n));
+
+            System.out.println("~");
+        }
+    }
 }
-    } 
-} 
 // } Driver Code Ends
 
 
-//User function Template for Java
-
-class Solution{
-    static String lookandsay(int n) {
-        //your code here
+class Solution {
+    public String countAndSay(int n) {
+        // code here
         
-        if(n == 1)
-            return "1";
-
-        StringBuilder previous = new StringBuilder("1");
-
+        String result = "1";
+        
         for(int i = 1; i < n; i++)
         {
-            StringBuilder current = new StringBuilder();
-            char ch = previous.charAt(0);
-            int count = 0;
-            for(int j = 0; j < previous.length();j++)
+            StringBuffer next = new StringBuffer();
+            int count = 1;
+        
+            for(int j = 1; j < result.length(); j++)
             {
-                if(ch == previous.charAt(j))
-                {
+                if(result.charAt(j) == result.charAt(j - 1))
                     count++;
-                }
                 else
                 {
-                    current.append(String.valueOf(count));
-                    current.append(ch);
-                    ch = previous.charAt(j);
+                    next.append(count).append(result.charAt(j - 1));
                     count = 1;
                 }
             }
-            current.append(String.valueOf(count));
-            current.append(ch);
-            previous = current;
+            next.append(count).append(result.charAt(result.length() - 1));
+            result = next.toString();
         }
-        return previous.toString();
+        
+        return result;
     }
 }
-
