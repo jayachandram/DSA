bdb377a9bafc31f2c01e5003ae7b33c197c20816
Jaya Chandra Mandapaka <153902034+jayachandram@users.noreply.github.com>
2025-05-30 22:55:15 +0530

    Time: 11 ms (91.17%), Space: 56.9 MB (95.41%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
@@ -1,47 +1,32 @@
 class Solution {
     public int closestMeetingNode(int[] edges, int node1, int node2) {
-
-        int n = edges.length;
-        int[][] distance = new int[n][2];
-        Queue<int[]> queue = new LinkedList<>();
-
-        for(int i = 0; i < n; i++)
-            distance[i][0] = distance[i][1] = -1;
+        int[] dist1 = getDistances(edges, node1);
+        int[] dist2 = getDistances(edges, node2);
         
-        distance[node1][0] = 0;
-        distance[node2][1] = 0;
-
-        queue.add(new int[]{node1, 1});
-        queue.add(new int[]{node2, 2});
-
-        while(!queue.isEmpty())
-        {
-            int node = queue.peek()[0];
-            int type = queue.poll()[1];
-            int neighbor = edges[node];
-
-            if(neighbor != -1 && distance[neighbor][type - 1] == -1)
-            {
-                distance[neighbor][type - 1] = distance[node][type - 1] + 1;
-                queue.add(new int[]{neighbor, type});
-            }
-        }
-
-        int commanNode = -1;
-        int minDistance = 1000000;
-
-        for(int i = 0; i < n; i++)
-        {
-            if(distance[i][0] != -1 && distance[i][1] != -1)
-            {
-                int max = Math.max(distance[i][0], distance[i][1]);
-                if(max < minDistance)
-                {
-                    minDistance = max;
-                    commanNode = i;
+        int minDist = Integer.MAX_VALUE;
+        int result = -1;
+        
+        for (int i = 0; i < edges.length; i++) {
+            if (dist1[i] != -1 && dist2[i] != -1) {
+                int maxDist = Math.max(dist1[i], dist2[i]);
+                if (maxDist < minDist) {
+                    minDist = maxDist;
+                    result = i;
                 }
             }
         }
-        return commanNode;
+        return result;
+    }
+
+    private int[] getDistances(int[] edges, int start) {
+        int n = edges.length;
+        int[] dist = new int[n];
+        Arrays.fill(dist, -1);
+        int d = 0, node = start;
+        while (node != -1 && dist[node] == -1) {
+            dist[node] = d++;
+            node = edges[node];
+        }
+        return dist;
     }
 }
