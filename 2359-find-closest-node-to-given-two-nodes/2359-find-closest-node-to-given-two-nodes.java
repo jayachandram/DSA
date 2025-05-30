45f8587c63f314b4c8fedc6eeff7e82d2dff30af
Jaya Chandra Mandapaka <153902034+jayachandram@users.noreply.github.com>
2025-05-30 22:59:05 +0530

    Time: 32 ms (26.85%), Space: 56.8 MB (95.41%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
@@ -1,32 +1,93 @@
+// We can optimize this in terms of optimization by reducing teh queue since there is only one out going edge from each node insted of using a queue we cam use simple while loop like we did in linked list. also we can use two seperate array insted of 2d array not much heping but looks cleaner.
+
+// class Solution {
+//     public int closestMeetingNode(int[] edges, int node1, int node2) {
+
+//         int n = edges.length;
+//         int[][] distance = new int[n][2];
+//         Queue<int[]> queue = new LinkedList<>();
+
+//         for(int i = 0; i < n; i++)
+//             distance[i][0] = distance[i][1] = -1;
+        
+//         distance[node1][0] = 0;
+//         distance[node2][1] = 0;
+
+//         queue.add(new int[]{node1, 1});
+//         queue.add(new int[]{node2, 2});
+
+//         while(!queue.isEmpty())
+//         {
+//             int node = queue.peek()[0];
+//             int type = queue.poll()[1];
+//             int neighbor = edges[node];
+
+//             if(neighbor != -1 && distance[neighbor][type - 1] == -1)
+//             {
+//                 distance[neighbor][type - 1] = distance[node][type - 1] + 1;
+//                 queue.add(new int[]{neighbor, type});
+//             }
+//         }
+
+//         int commanNode = -1;
+//         int minDistance = 1000000;
+
+//         for(int i = 0; i < n; i++)
+//         {
+//             if(distance[i][0] != -1 && distance[i][1] != -1)
+//             {
+//                 int max = Math.max(distance[i][0], distance[i][1]);
+//                 if(max < minDistance)
+//                 {
+//                     minDistance = max;
+//                     commanNode = i;
+//                 }
+//             }
+//         }
+//         return commanNode;
+//     }
+// }
+
 class Solution {
     public int closestMeetingNode(int[] edges, int node1, int node2) {
-        int[] dist1 = getDistances(edges, node1);
-        int[] dist2 = getDistances(edges, node2);
-        
-        int minDist = Integer.MAX_VALUE;
-        int result = -1;
-        
-        for (int i = 0; i < edges.length; i++) {
-            if (dist1[i] != -1 && dist2[i] != -1) {
-                int maxDist = Math.max(dist1[i], dist2[i]);
-                if (maxDist < minDist) {
-                    minDist = maxDist;
-                    result = i;
+
+        int n = edges.length;
+        int[][] distance = new int[n][2];
+
+        for(int i = 0; i < n; i++)
+            distance[i][0] = distance[i][1] = -1;
+
+        int node = node1;
+        int temp = 0;
+        while(node != -1 && distance[node][0] == -1)
+        {
+           distance[node][0] = temp++;
+           node = edges[node];
+        }
+
+        node = node2;
+        temp = 0;
+        while(node != -1 && distance[node][1] == -1)
+        {
+           distance[node][1] = temp++;
+           node = edges[node];
+        }
+
+        int commanNode = -1;
+        int minDistance = 1000000;
+
+        for(int i = 0; i < n; i++)
+        {
+            if(distance[i][0] != -1 && distance[i][1] != -1)
+            {
+                int max = Math.max(distance[i][0], distance[i][1]);
+                if(max < minDistance)
+                {
+                    minDistance = max;
+                    commanNode = i;
                 }
             }
         }
-        return result;
-    }
-
-    private int[] getDistances(int[] edges, int start) {
-        int n = edges.length;
-        int[] dist = new int[n];
-        Arrays.fill(dist, -1);
-        int d = 0, node = start;
-        while (node != -1 && dist[node] == -1) {
-            dist[node] = d++;
-            node = edges[node];
-        }
-        return dist;
+        return commanNode;
     }
 }
