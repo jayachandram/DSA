af8dabd6e4c895acf7dfa0c08faef241b08ed9ba
Jaya Chandra Mandapaka <153902034+jayachandram@users.noreply.github.com>
2025-06-09 21:01:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete Middle of Linked List/delete-middle-of-linked-list.java
@@ -0,0 +1,29 @@
+/*
+class Node {
+    int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+
+class Solution {
+    Node deleteMid(Node head) {
+        // This is method only submission.
+        // You only need to complete the method.
+        if(head.next == null) return null;
+
+        Node slow = head, fast = head, prev = null;
+
+        while(fast != null && fast.next != null)
+        {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        prev.next = prev.next.next;
+
+        return head;
+
+    }
+}
